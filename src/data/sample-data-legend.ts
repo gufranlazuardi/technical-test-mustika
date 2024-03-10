8e9cb4465e203be20c25f2c785fb9244cca84e84
gufranlazuardi <gufranlazuardi@gmail.com>
2024-03-10 15:17:24 +0700

    refactor: sample data

--- src/data/sample-data-legend.ts
+++ src/data/sample-data-legend.ts
@@ -12,39 +12,39 @@ interface ILegend {
   [key: string]: ILegendItem;
 }
 
-const legend: ILegend  = {
+const legend: ILegend = {
   all: {
-    pt: "Semua AP",
+    city: "all",
     average: "34%",
-      total: 10,
-      percentile100: 4,
-      percentile89: 8,
-      percentile69: 20,
+    total: 10,
+    percentile100: 4,
+    percentile89: 8,
+    percentile69: 20,
   },
   mpu: {
-    city: 'pandeglang',
-      average: "34%",
-      total: 10,
-      percentile100: 4,
-      percentile89: 8,
-      percentile69: 20,
+    city: "pandeglang",
+    average: "34%",
+    total: 10,
+    percentile100: 4,
+    percentile89: 8,
+    percentile69: 20,
   },
   lan: {
-    city: 'banyuwangi',
-      average: "24%",
-      total: 20,
-      percentile100: 2,
-      percentile89: 28,
-      percentile69: 22,
+    city: "banyuwangi",
+    average: "24%",
+    total: 20,
+    percentile100: 2,
+    percentile89: 28,
+    percentile69: 22,
   },
   ksm: {
-    city: 'garut',
-      average: "70%",
-      total: 30,
-      percentile100: 24,
-      percentile89: 18,
-      percentile69: 25,
+    city: "garut",
+    average: "70%",
+    total: 30,
+    percentile100: 24,
+    percentile89: 18,
+    percentile69: 25,
   },
 };
 
-export default legend
+export default legend;
