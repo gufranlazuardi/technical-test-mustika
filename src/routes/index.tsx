b692d494462f79ce835da17c5af3c9b76bedbe76
gufranlazuardi <gufranlazuardi@gmail.com>
2024-03-10 14:05:36 +0700

    feat: routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import Home from "@/pages";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+export default function Router() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
