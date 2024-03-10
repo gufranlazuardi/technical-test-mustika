36a5fcaa9eb4775cffaa8e5fa2395ecd9955e6df
gufranlazuardi <gufranlazuardi@gmail.com>
2024-03-10 14:05:27 +0700

    feat: index page

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import Layout from "@/components/Layout";
+import Maps from "@/components/Maps";
+
+import Tabel from "@/components/Tabel";
+
+const Home = () => {
+  return (
+    <Layout>
+      <div className="gap-4 flex flex-col">
+        <h1 className="text-2xl font-bold">Pelayanan PPL</h1>
+        <div className="flex justify-center bg-white py-2">
+          <p className="text-sm">Tanggal review 18-12-2020 - 18-12-2023</p>
+        </div>
+        <Maps />
+
+        <Tabel />
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
