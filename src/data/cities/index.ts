6a39dea9379030e86c734bb4ff26b4fa09f0a688
gufranlazuardi <gufranlazuardi@gmail.com>
2024-03-10 14:05:17 +0700

    feat: sample data

---
+++ src/data/cities/index.ts
@@ -0,0 +1,6 @@
+import malang from './malang'
+import banyuwangi from './banyuwangi'
+import garut from './garut'
+import pandeglang from './pandeglang'
+
+export default { malang, banyuwangi, garut, pandeglang }
